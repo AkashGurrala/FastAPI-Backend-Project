2ea3a18b0c0e0851340d9a416756d8af9d86fc17
AkashGurrala <akashgurrala@gmail.com>
2026-03-02 14:21:57 +0530

    Day 25: Migrated the database from in-memory to Postgres and made changes in the code accordingly

--- app/data/store.py
+++ app/data/store.py
@@ -1,3 +1,5 @@
+from itertools import product
+from unittest import result
 from app.schemas.product_schema import Product
 from app.core.logger import logger
 from app.db.connection import get_db_connection
@@ -6,81 +8,158 @@ from app.db.connection import get_db_connection
 # Next: Will be replaced by postgreSQL queries
 
 
-products = [
-    Product(id=1, name = 'Erwin Smith', strengths='Leadership and Conviction'),
-    Product(id=2, name = 'Gold D Roger', strengths = 'Freedom and Strength'),
-    Product(id=3, name='Asta', strengths = 'Hardwork and Optimism'),
-    Product(id=4, name = 'Monkey D Luffy', strengths = 'Freedom and resilence'),
-    Product(id=5, name = 'Gojo Satoru', strengths = 'The Strongest'),
-    Product(id=6, name = 'Tsukasa', strengths = 'Strength and Skilled Fighter')
-]
-
 def get_all_products():
     try:
+        conn = None
+        cursor = None
         conn = get_db_connection()
         cursor = conn.cursor()
         cursor.execute('SELECT * FROM products;')
-        result = cursor.fetchall()
+        rows = cursor.fetchall()
+        result = []
+        for row in rows:
+            product = Product(
+                id= row[0],
+                name= row[1],
+                strengths= row[2]
+            )
+            result.append(product)
         print("Database Fetch Successful")
-        cursor.close()
-        conn.close()
         return result
     
     except Exception as e:
         print("Database operation failed: ", str(e))
         print("Since DB operation is failed, using in-memory list to commplete operation")
         return products
-
-def get_all_products_from_db():
-    try:
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        cursor.execute('SELECT * FROM products;')
-        rows = cursor.fetchall()
-        print("Database fetch Successful: ")
-        cursor.close()
-        conn.close()
-        return rows
-
-    except Exception as e:
-        print("Database operation failed:", str(e))
-        return[]
+    
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
 
 
 def product_by_id(id: int):
-    
-    product = [p for p in products if p.id == id]
-    return product
+    try:
+        conn = None
+        cursor = None
+        conn=get_db_connection()
+        cursor=conn.cursor()
 
+        cursor.execute('SELECT * FROM products where id=%s;', (id,))
+        result=cursor.fetchall()
+        print("Database operation successful:")
+        return result
+    
+    except Exception as e:
+        print("Database Operation Failed:", str(e))
+        return []
+    
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
+'''
 def product_by_name(name):
+    try:
+        conn = None
+        cursor = None
+        conn=get_db_connection()
+        cursor=conn.cursor()
 
-    product = [p for p in products if p.name.lower() == name.lower()]
-    return product
-
+        cursor.execute('SELECT * FROM products where name ilike %s;', (name,))
+        result = cursor.fetchone()
+        return result
+    
+    except Exception as e:
+        print("Database Operation Failed:", str(e))
+        return []
+    
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
+'''
 def count_products():
-    count = len(products)
-    return count
+    try:
+        conn = None
+        cursor = None
+        conn=get_db_connection()
+        cursor=conn.cursor()
 
-def add_product(request_id, product):
+        cursor.execute('SELECT COUNT (*) FROM products;')
+        result=cursor.fetchone()[0]
+        logger.info("Database operation successful:")
+        return result
     
-    new_id=len(products)+1
-    new_product = Product(
-        id = new_id,
-        name = product.name,
-        strengths= product.strengths
-    )
+    except Exception as e:
+        logger.error("Database Operation Failed:", str(e))
+        return []
     
-    products.append(new_product)
-    logger.info(f"[{request_id}] Product is successfully added to the database")
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
 
-    return new_product
+def add_product(product):
+
+    try:
+        conn = None
+        cursor = None
+        conn=get_db_connection()
+        cursor=conn.cursor()
+        cursor.execute("INSERT INTO products (name, strengths) values (%s,%s);",(product.name, product.strenghts))
+        conn.commit()
+        cursor.execute("SELECT * FROM products WHERE name = %s", (product.name,))
+        result = cursor.fetchone()
+        result = Product(
+            id = result[0],
+            name = result[1],
+            strengths = result[2] 
+        )
+        print("Database operation successful:")
+        return result
+    
+    except Exception as e:
+        print("Database Operation Failed:", str(e))
+        return []
+    
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
 
 def search_products(name: str):
+    try:
+        conn = None
+        cursor = None
+        conn=get_db_connection()
+        cursor=conn.cursor()
+
+        cursor.execute("SELECT * FROM products where name ilike %s;", (f"%{name}%",))
+        product_list=cursor.fetchall()
+        print("Database fetch successful:")
+        result=[]
+        for row in product_list:
+            product = Product(
+                id = row[0],
+                name = row[1],
+                strengths = row[2]
+            )
+            result.append(product)
+
+        return result
     
-    search_list=[]
-    for p in products:
-        if name in p.name:
-            search_list.append(p)
-
-    return search_list
-
+    except Exception as e:
+        print("Database Operation Failed:", str(e))
+        return []
+    
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
--- app/services/product_service.py
+++ app/services/product_service.py
@@ -1,6 +1,6 @@
 from app.logic import sort_products
 from app.core.logger import logger
-from app.data.store import get_all_products_from_db, product_by_id, count_products, add_product, search_products
+from app.data.store import get_all_products, product_by_id, count_products, add_product, search_products
 from app.services.exceptions import InvalidInputException, NoProductFoundException
 
 def get_singleproduct_by_id(request_id, id):
@@ -34,7 +34,7 @@ def get_filtered_products(request_id, min_id, sort_by_id, name_contains, limit, 
         logger.warning("Invalid offset value recieved")
         raise InvalidInputException("Invalid offset value recieved. Offset value must be equal to or greater than 0")
 
-    all_products = get_all_products_from_db()
+    all_products = get_all_products()
 
     filtered = sort_products(all_products, min_id, sort_by_id, name_contains, limit, offset)
     
@@ -49,13 +49,13 @@ def products_count(request_id):
 def create_product(request_id, product):
     
     logger.info(f"[{request_id}] Adding new product")
-    products_list = get_all_products_from_db()
+    products_list = get_all_products()
     product.name = product.name.strip()
     for p in products_list:
         if p.name.lower() == product.name.lower():
             raise InvalidInputException("Product name already exists")
 
-    new_product = add_product(request_id, product)
+    new_product = add_product(product)
     return new_product
 
 
